41c1a1ae7d72e07c9aa5f1551eea3b8b95d7772e
shaqaotmeal <mohamadihsanabs@gmail.com>
2024-12-01 22:27:34 +0100

    Create config.py
    configuration du jeu

---
+++ TPS/projet_POO/config.py
@@ -0,0 +1,27 @@
+DEFAULT_CELL_SIZE = 40
+CELL_SIZE= 40
+# Image paths for tiles
+GRASS_IMAGE_PATH = "assets/grass.png"
+WATER_IMAGE_PATH = "assets/water.png"
+LAVA_IMAGE_PATH = "assets/lava.png"
+SOIL_IMAGE_PATH = "assets/soil.png"
+
+# Colors
+BLUE = (0, 0, 255)
+RED = (255, 0, 0)
+
+# Assign distinct colors for each character
+PLAYER1_IMAGES = {
+    "T": "assets/jayce.png",
+    "A": "assets/vi.png",
+    "M": "assets/jinx.png",
+    "G": "assets/ekko.png"
+}
+
+PLAYER2_IMAGES = {
+    "T": "assets/jayce.png",
+    "A": "assets/vi.png",
+    "M": "assets/jinx.png",
+    "G": "assets/ekko.png"
+}
+
