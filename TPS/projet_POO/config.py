09b29bc14c1307a6a0d4efbac39ab195523d3d81
shaqaotmeal <mohamadihsanabs@gmail.com>
2024-12-07 19:09:54 +0100

    Update config.py

--- TPS/projet_POO/config.py
+++ TPS/projet_POO/config.py
@@ -1,16 +1,17 @@
-DEFAULT_CELL_SIZE = 40
-CELL_SIZE= 40
-# Image paths for tiles
+CELL_SIZE = 35
 GRASS_IMAGE_PATH = "assets/grass.png"
 WATER_IMAGE_PATH = "assets/water.png"
 LAVA_IMAGE_PATH = "assets/lava.png"
 SOIL_IMAGE_PATH = "assets/soil.png"
+ROCK1_IMAGE_PATH = "assets/rock.png"
+ROCK2_IMAGE_PATH = "assets/rock2.png"
+ROCK3_IMAGE_PATH = "assets/rock3.png"
+DEAD_GRASS = "assets/dead_grass.png"
+DEAD_SOIL = "assets/dead_soil.png"
+SWORD = "assets/sword.png"
+ARMOR = "assets/armor.png"
+BOOTS = "assets/boots.png"
 
-# Colors
-BLUE = (0, 0, 255)
-RED = (255, 0, 0)
-
-# Assign distinct colors for each character
 PLAYER1_IMAGES = {
     "T": "assets/jayce.png",
     "A": "assets/vi.png",
