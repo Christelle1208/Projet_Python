26198610e03b6405542bbb17dec2ab6c56b448c1
shaqaotmeal <mohamadihsanabs@gmail.com>
2024-12-13 16:07:55 +0100

    Update positions.py

--- TPS/projet_POO/positions.py
+++ TPS/projet_POO/positions.py
@@ -1,13 +1,20 @@
 import random
 
-def random_position(ligne, colonne, liste):
-    x = random.choice(ligne)
-    y = random.choice(colonne)
-    while (x,y) in liste:
-        print("doublon")
-        x = random.choice(ligne)
-        y = random.choice(colonne)
-    return (x,y)
+    def random_position(ligne, colonne, taken_positions):
+            """Génère une position aléatoire unique."""
+            x = random.choice(ligne)
+            y = random.choice(colonne)
+            while (x, y) in taken_positions:
+                print("doublon")
+                x = random.choice(ligne)
+                y = random.choice(colonne)
+            return (x, y)
+
+        player1_ligne = range(1, 5)  
+        player1_colonne = range(1, 5)  
+
+        player2_ligne = range (16,20)
+        player2_colonne = range(16,20)
 
 # liste = []
 # ligne = [1,2,3,4,5]
@@ -15,4 +22,3 @@ def random_position(ligne, colonne, liste):
 # for i in range(4):
 #     liste.append(random_position(ligne, colonne, liste))
 # print (liste)
-
