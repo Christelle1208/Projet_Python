0c869a720a999cc1f8053dd638c8cc4fca5bd000
fouadsw <fouadboudhane3@gmail.com>
2024-12-13 23:58:53 +0100

    update abilities.py

--- TPS/projet_POO/abilities.py
+++ TPS/projet_POO/abilities.py
@@ -30,3 +30,21 @@ class Bomb(Ability):
                 abs(dx) + abs(dy) <= 3
             )
         ]
+# dégats sur la zone affectée
+        for unit in game.units:
+            if (unit.x, unit.y) in affected_tiles:
+                distance = abs(unit.x - center_x) + abs(unit.y - center_y)
+                damage = max(0, 30 - (distance * 5))  # plus il est loin, moin les dégats sont forts
+                unit.take_damage(damage)
+                if unit.hp <= 0:
+                    print_f(f"{unit.name} a éte vaincu !")
+                    if game.map[unit.y][unit.x].tile_type == "grass":
+                        game.map[unit.y][unit.x].tile_type = "dead_grass"
+                    if game.map[unit.y][unit.x].tile_type == "soil":
+                        game.map[unit.y][unit.x].tile_type = "dead_soil"
+                    game.units.remove(unit)
+                    
+
+        print_f(f"{user.name} a utiliser l'habilité bombe.")
+        user.has_acted = True
+        game.clear_affected_tiles()  # supprime la fumée après utilisation 
