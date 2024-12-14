6b4c6bc4eb62430a78dd5139900f84b36437f30f
Christelle1208 <christelle.nollet@laposte.net>
2024-12-14 20:31:42 +0100

    fichier main

---
+++ TPS/projet_POO/main.py
@@ -0,0 +1,44 @@
+import pygame
+import json
+from game import Game
+from config import CELL_SIZE, MAP1, MAP2, MAP3
+from print_f import *
+from positions import *
+from menu import *
+
+def main():
+    map_choice = main_menu()
+    if map_choice == 1:
+        map_path = MAP1
+    elif map_choice == 2:
+        map_path = MAP2
+    elif map_choice == 3:
+        map_path = MAP3
+
+    with open(map_path) as f:
+        data = json.load(f)
+        num_rows = len(data["layers"][0]["data"])
+        num_columns = len(data["layers"][0]["data"][0]) if num_rows > 0 else 0
+
+    width = num_columns * CELL_SIZE 
+    height = num_rows * CELL_SIZE
+    pygame.init()
+    screen = pygame.display.set_mode((width, height))
+    pygame.display.set_caption("projet poo")
+
+    game = Game(screen,map_choice)
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.KEYDOWN:
+                game.handle_key(event.key)
+
+        game.update()
+        pygame.display.flip()
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
