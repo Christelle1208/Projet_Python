de2c5400eaf4ae2e29ab93323d8dcb19a936ff56
shaqaotmeal <mohamadihsanabs@gmail.com>
2024-12-14 20:53:42 +0100

    Update game.py

--- TPS/projet_POO/game.py
+++ TPS/projet_POO/game.py
@@ -63,115 +63,32 @@ class Game:
             unit.set_game(self)
 
 
-
-
-        
-
-        def generate_positions(rows):
-            """Génère trois positions aléatoires dans des lignes définies."""
-            positions = set()  # Utiliser un set pour éviter les doublons
-            while len(positions) < 3:
-                x = random.choice(rows)  # Ligne autorisée
-                y = random.randint(0, GRID_SIZE - 1)  # Colonne aléatoire
-                positions.add((x, y))
-            return list(positions)
-
-
-        self.screen = screen
-        print("Types : Tank, Assassin, Mage, Archer_poison, Archer_electricite")
-        type_joueur,team=["","",""],'player'
-        player1_pos = generate_positions(rows=[0, 1])
-        for i in range(3):
-            type_joueur[i]=str(input(f"Joueur 1 : Choisissez un type de personnage pour l'unité {i+1}: "))
-        self.player_units = [Personnage(type_joueur[0],team,player1_pos[0]).character(),
-                             Personnage(type_joueur[1],team,player1_pos[1]).character(),
-                             Personnage(type_joueur[2],team,player1_pos[2]).character()]
-        
-        x,y=random.randint(1,7),random.randint(1,7)
-        type_ennemi,team=["","",""],'enemy'
-        player2_pos = generate_positions(rows=[6, 7])
-        for i in range(3):
-            type_ennemi[i]=str(input(f"Joueur 2 : Choisissez un type de personnage pour l'unité {i+1}: "))
-        self.enemy_units = [Personnage(type_ennemi[0],team,player2_pos[0]).character(),
-                            Personnage(type_ennemi[1],team,player2_pos[1]).character(),
-                            Personnage(type_ennemi[2],team,player2_pos[2]).character()]
-
+ 
+    def load_map(self, data):
+            """chargement des tiles"""
+            num_rows = len(data["layers"][0]["data"])
+            num_columns = len(data["layers"][0]["data"][0]) if num_rows > 0 else 0
+            self.cell_size = min(self.screen.get_width() // num_columns, self.screen.get_height() // num_rows)
     
-    def flip_display(self):
-        """Affiche le jeu."""
-
-        # Efface l'écran
-        self.screen.fill(BLACK)
-
-        # Affiche les cases accessibles pour l'unité sélectionnée
-        for unit in self.player_units + self.enemy_units:
-            if unit.accessible_tiles:  # Vérifie si l'unité a des cases accessibles stockées
-                for tile_x, tile_y in unit.accessible_tiles:
-                    rect = pygame.Rect(tile_x * CELL_SIZE, tile_y * CELL_SIZE, CELL_SIZE, CELL_SIZE)
-                    pygame.draw.rect(self.screen, (50, 50, 200,128), rect)  # Bleu pour les cases accessibles
-
-        # Affiche la grille par-dessus
-        for x in range(0, WIDTH, CELL_SIZE):
-            for y in range(0, HEIGHT, CELL_SIZE):
-                rect = pygame.Rect(x, y, CELL_SIZE, CELL_SIZE)
-                pygame.draw.rect(self.screen, WHITE, rect, 1)  # Ligne blanche pour la grille
-
-        # Affiche les unités
-        for unit in self.player_units + self.enemy_units:
-            unit.draw(self.screen)
-
-        # Rafraîchit l'écran
-        pygame.display.flip()
-
-
-
-def main():
-
-    # Initialisation de Pygame
-    pygame.init()
-
-    # Instanciation de la fenêtre
-    screen = pygame.display.set_mode((WIDTH, HEIGHT))
-    pygame.display.set_caption("Mon jeu de stratégie")
-
-    # Instanciation du jeu
-    game = Game(screen)
-
-    # Boucle principale du jeu
-    while True:
-        game.handle_player_turn(1) # Tour du joueur 1
-        game.handle_player_turn(2) # Tour du joueur 2
-
-
-if __name__ == "__main__":
-    main()
- 
- 
-def load_map(self, data):
-        """chargement des tiles"""
-        num_rows = len(data["layers"][0]["data"])
-        num_columns = len(data["layers"][0]["data"][0]) if num_rows > 0 else 0
-        self.cell_size = min(self.screen.get_width() // num_columns, self.screen.get_height() // num_rows)
-
-
-        for y, row in enumerate(data["layers"][0]["data"]):
-            for x, tile_id in enumerate(row):
-                if tile_id == 3:  # BOUE
-                    self.hidden_mud.add((x, y))
-
-        
-        for y, row in enumerate(data["layers"][0]["data"]):
-            tile_row = []
-            for x, tile_id in enumerate(row):
-                tile_type = (
-                "wall" if tile_id == 0 else
-                "grass" if tile_id == 1 else
-                "water" if tile_id == 2 else
-                "mud" if tile_id == 3 else
-                "soil" if tile_id == 4 else
-                "rock" if tile_id == 5 else
-                "grass"
-                )
-                tile = Tile(x, y, tile_type, self.cell_size, self.hidden_mud)
-                tile_row.append(tile)
-            self.map.append(tile_row)
+    
+            for y, row in enumerate(data["layers"][0]["data"]):
+                for x, tile_id in enumerate(row):
+                    if tile_id == 3:  # BOUE
+                        self.hidden_mud.add((x, y))
+    
+            
+            for y, row in enumerate(data["layers"][0]["data"]):
+                tile_row = []
+                for x, tile_id in enumerate(row):
+                    tile_type = (
+                    "wall" if tile_id == 0 else
+                    "grass" if tile_id == 1 else
+                    "water" if tile_id == 2 else
+                    "mud" if tile_id == 3 else
+                    "soil" if tile_id == 4 else
+                    "rock" if tile_id == 5 else
+                    "grass"
+                    )
+                    tile = Tile(x, y, tile_type, self.cell_size, self.hidden_mud)
+                    tile_row.append(tile)
+                self.map.append(tile_row)
